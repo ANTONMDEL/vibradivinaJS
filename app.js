f703a30888706157b3903452cbd1f83048e40ee8
antondl <marcoanton.deluca@gmail.com>
2021-03-25 18:26:23 -0300

    Ejercicio clase 3 For DO WHILE y SWITCH

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+for (let i = 1; i <= 3; i++) {
+    let socio = prompt("ingrese su nombre y apellido");
+    if (socio == "Marco De Luca") {
+        alert("Hola" + " Dr" + " " + socio + " " + "sos el primero !");
+        break;
+    }
+    alert("Hola" + " " + socio + " " + "su turno es el " + i);
+
+}
+
+
+let palabraClave = prompt(" ingrese su codigo clave !")
+while (palabraClave != "1234") {
+    switch (palabraClave) {
+        case "luis":
+            console.log("Bienvenido" + " " + "luis")
+            break;
+        case "marco":
+            console.log("Bienvenido" + " " + "marco")
+            break;
+
+        default:
+            console.log("chau")
+            break;
+    }
+    palabraClave = prompt(" ingrese su codigo clave !");
+}
+
+
+
+/*  ENTREGA UNO Y DOS 
+
 let sintomas = prompt("Tiene sintomas ? Ingrese sintomas");
 let sintomaUno = "fiebre";
 let sintomaDos = "tos";
@@ -16,3 +48,4 @@ else {
     alert("Esos sintomas no son de COVID");
 }
 
+*/
