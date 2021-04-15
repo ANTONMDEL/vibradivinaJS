b606f469bb4ee1d619d5e55a153c42d8ced81e0a
antondl <marcoanton.deluca@gmail.com>
2021-04-15 19:12:07 -0300

    Re-entrega del desafio Primera entrega del proyecto final

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,14 +1,3 @@
-/*Practica Local y Session storage
-localStorage.setItem("nombre", "Marco")
-localStorage.setItem("edad", 19);
-
-let name = localStorage.getItem(nombre);
-let aye = localStorage.getItem(edad);
-console.log(typeof eye);
-console.log(typeof name);
-*/
-
-
 
 const usuario1 = {
     id: 1,
@@ -39,60 +28,28 @@ const usuario4 = {
 
 array.push(usuario4);
 
-//El objetivo es mostrar el ID de cada usuario y que se imprima quien pago y quien no.
-for (const value of array) {
-    console.log(value.id);
-    console.log(value.pago);
-}
 
-//Funcion para ordenes el elemento "puntos" de mi array, de mayor a menor. 
-function puntajeUsuario(a, b) {
-    return b.puntos - a.puntos;
-}
-let puntajeU = array.sort(puntajeUsuario);
-console.log(puntajeU);
+const pagoU= array ;
 
-
-
-/* prueba de objetos para practicar 
-class Persona {
-    constructor( nombre, edad, calle){
-this.nombre= nombre
-this.edad = edad
-this.calle= calle
-    }
-    hablar(){
-      console.log("Hola soy" + " " + this.nombre);
+function pagoss(pagoU) {
+ 
+    for (let valor of pagoU){
+       if (valor.pago == false) {
+           console.log(valor.id)
+           console.log("no pago");
+       }
+       else{
+        console.log(valor.id)
+           console.log("pago")
+       }
     }
 }
-const personaDos = new Persona ("carlos", 19 , "Carlos magno");
-const personaTres = new Persona ("luis", 109 , "Carlos magno");
-personaDos.hablar();
-personaTres.hablar();
- */
+pagoss(pagoU);
 
-// ENTREGA 6a ARRAY 
-const usuario = [123] ;
-let gasto = parseInt(prompt('Cuanto queres gastar del 1 al 5 ?'));
-
-for (let index = 0; index < gasto; index++) {
-    let nombre = prompt("ingrese su nombre completo");
-    let usuarioNumero =  parseInt((prompt("ingrese su numero de usuario")));
-    let descuento = prompt("Tiene codigo de descuento ?");
- usuario.push(nombre);
- usuario.push(usuarioNumero)
-usuario.push(descuento);
-}
-
-const descuentoUtilizado = usuario.slice(3,4);
-
-
-console.log(usuario.length);
-console.log(usuario.join(" "));
-console.log(descuentoUtilizado); 
 
 
 // ENTREGA 5 OBJETOS  
+
 const Reiki = new Cursos ("Reiki", 300 , 3 );
 console.log(Reiki.nombre);
 console.log(Reiki.costo);
@@ -113,97 +70,11 @@ console.log(Registros.duracion);
 Registros.bienvenida();
 
 
-// ENTREGA 4 
-var nombre = prompt("ingrese su nombre para ingresar !")
-var cumple = prompt(" Ingrese el nombre del anfitrion")
+//Practica Local y Session storage
+localStorage.setItem("nombre", "Marco")
+localStorage.setItem("edad", 19);
 
-function boliche(nombre) {
-    console.log("Hola" + " " + nombre);
-
-}
-let edad = prompt("ingrese su edad !");
-
-function mayor(edad) {
-    if (edad >= 18) {
-        return (alert("Bienvenido" + " " + nombre))
-    } else if (edad == 17) {
-        return (alert("No puede pasar, volve cuando cumplas 18"))
-    } else {
-        return (alert("No puede pasar" + " " + nombre))
-    }
-}
-
-function feliz(cumple) {
-    switch (cumple) {
-        case "Alejandro":
-            console.log("Invitado de " + " " + "Alejandro")
-            break;
-        case "Marco":
-            console.log("Invitado de" + " " + "Marco")
-            break;
-        default:
-            break;
-    }
-}
-
-boliche(nombre);
-mayor(edad);
-feliz(); 
-
-
-
-
-
-/*  ENTREGA 3
-for (let i = 1; i <= 3; i++) {
-    let socio = prompt("ingrese su nombre y apellido");
-    if (socio == "Marco De Luca") {
-        alert("Hola" + " Dr" + " " + socio + " " + "sos el primero !");
-        break;
-    }
-    alert("Hola" + " " + socio + " " + "su turno es el " + i);
-
-}
-
-
-
-let palabraClave = prompt(" ingrese su codigo clave !")
-while (palabraClave != "1234") {
-    switch (palabraClave) {
-        case "luis":
-            console.log("Bienvenido" + " " + "luis")
-            break;
-        case "marco":
-            console.log("Bienvenido" + " " + "marco")
-            break;
-
-        default:
-            console.log("chau")
-            break;
-    }
-    palabraClave = prompt(" ingrese su codigo clave !");
-}
-
-*/
-
-/*  ENTREGA 1 y 2 
-
-let sintomas = prompt("Tiene sintomas ? Ingrese sintomas");
-let sintomaUno = "fiebre";
-let sintomaDos = "tos";
-let sintomaTres = "Dolor de cabeza";
-let sintomaCuatro = "falta de aire";
-let sintomaX = "otro";
-
-if (((sintomas == sintomaUno) || (sintomas == sintomaDos) || (sintomas == sintomaTres) || (sintomas == sintomaCuatro))) {
-    alert("Llama al 0800-covid")
-} else if (sintomas == "") {
-    alert("No ingreso informacion");
-}  else if (sintomas == sintomaX){
-    alert("Si tiene sintomas como tos, fiebre, dolor de cabeza o falta de aire, llamar al 0800-covid")
-}
-else {
-    alert("Esos sintomas no son de COVID");
-}
-
-*/
+let name = localStorage.getItem(nombre);
+let aye = localStorage.getItem(edad);
+console.log(typeof eye);
+console.log(typeof name);
--- JavaScript/objects.js
+++ JavaScript/objects.js
@@ -10,4 +10,3 @@ function Cursos ( nombre , costo , duracion) {
 
 
 
-
